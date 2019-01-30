c29e7c23509e51b4f9fb4fe0d28483844ddf3651
ling-lian-hong <l_l_l_l_h_h@163.com>
2019-01-30 15:43:35 +0800

    Update NewBehaviourScript.cs

--- UI/NewBehaviourScript.cs
+++ UI/NewBehaviourScript.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour {
-
+public float a = 10;
 	// Use this for initialization
 	void Start () {
-		
+		Debug.Log(a);
 	}
 	
 	// Update is called once per frame
